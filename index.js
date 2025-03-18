b352766ebf3ca8cd477cc48de4390d7af987e66c
firozkhan4 <firozkhan907979@gmail.com>
2025-03-18 21:54:39 +0530

    new commmit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import "dotenv/config.js"
+import { fileURLToPath } from "url";
+import path from "path"
 import connectDB from "./config/dbConfig.js";
 
 // Import Controllers
@@ -9,14 +11,22 @@ import handleImages from "./controllers/imageController.js"
 
 const PORT = process.env.PORT || 8000
 
+// Get directory name in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express()
 const connect = connectDB()
 
 // Middlewares
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "views")));
 app.use(morgan("combined"));
 app.use("/uploads", express.static("uploads"))
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname))
+})
 app.use("/upload", handleUpload);
 app.use("/images", handleImages);
 
